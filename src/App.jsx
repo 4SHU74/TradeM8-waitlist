c550cb05cd1403cb1d171032fb544691216d42f8
iamashura <hackerz55org@gmail.com>
2026-01-12 11:49:44 +0100

    day one setting up ( react, vite , tailwind and shadnc) done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h1 className="text-3xl font-bold">Vibe Check! 🚀</h1>
+      <p className="text-muted-foreground">
+        If you see a styled button below, shadcn is working.
+      </p>
+
+      <div className="flex gap-5">
+        <Button variant="outline">james</Button>
+
+        <Button variant="primary">well</Button>
+        <Button variant="primary">Destroy</Button>
+      </div>
+    </div>
+  );
 }
-
-export default App;
