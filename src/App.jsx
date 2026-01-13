542c111fa5887fdb7a706d97d2a2ad77fa9ea23a
iamashura <hackerz55org@gmail.com>
2026-01-13 10:39:57 +0100

    nav bar done for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Navbar from "./personal blocks/Navbar.jsx";
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <h1 className="text-3xl font-bold">Vibe Check! 🚀</h1>
-      <p className="text-muted-foreground">
-        If you see a styled button below, shadcn is working.
-      </p>
-
-      <div className="flex gap-5">
-        <Button variant="outline">james</Button>
-
-        <Button variant="primary">well</Button>
-        <Button variant="primary">Destroy</Button>
-      </div>
+    <div className="mx-[60px]">
+      <Navbar />
     </div>
   );
 }
---
+++ src/personal blocks/Navbar.jsx
@@ -0,0 +1,31 @@
+import logo from "../assets/logo.png";
+
+function Navbar() {
+  return (
+    <nav className="w-full flex items-center justify-between px-8 py-4 bg-EDEEF0 my-3">
+      <div className="flex items-center gap-3 w-fit">
+        <img src={logo} className="w-16 h-16 rounded-lg object-contain" />
+        <span className="font-bold text-[#1A3B50] text-xl leading-none">
+          TradeM8
+        </span>
+      </div>
+
+      <button
+        className="
+          bg-[#0F832E]
+          text-white
+          px-6
+          py-2
+          rounded-lg
+          font-medium
+          hover:bg-[#0F832E]/90
+          h-[55px]
+          transition-all
+        "
+      >
+        Join the Waitlist
+      </button>
+    </nav>
+  );
+}
+export default Navbar;
